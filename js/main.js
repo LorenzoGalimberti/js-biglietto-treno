aea5845349b22fb8e96f9d7942a6734a94242b84
LorenzoGalimberti <lorenzogalimberti7996@gmail.com>
2023-07-03 13:47:25 +0200

    setup progetto

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero (con i prompt() in questo caso).
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo e questo richiederà un minimo di ricerca.).
+// Il risultato andrà visualizzato prima in console e poi possibilmente in un apposito elemento <p> del mio codice HTML.
+// BONUS:
+
+
+// 1) DICHIARARE INSERIRE A PROMPRT I DATI DELL' UTENTE e variabili utili 
+
+// 2) ASSEGNAZIONE CATEGORIA
+// 3) CONDIZIONI PER VEDERE SE APPLICARE UNO SCONTO 
+// 4) STAMPARE L' ELEMENTO A CONSOLE 
